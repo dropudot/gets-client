c91e0594b069ac7cf82a59bffa25a84da1d34cf4
dropudot <ruslan161rus@live.ru>
2024-01-30 16:23:57 +0300

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import "../app.css";
+</script>
+<header class="bg-cyan-500 h-16">
+    <div class="h-full flex">
+      <div class="flex w-full items-center justify-center">
+        <div class="text-white">
+          logo
+        </div> 
+      </div>
+      <div class="absolute right-0 align-middle h-16 text-white">
+          menu
+      </div>
+    </div>
+  </header>
+  <body class="flex flex-col bg-zinc-950 h-screen overflow-y-hidden">
+      <slot />
+  </body>
---
+++ src/routes/beer/+page.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+ import { Button } from 'flowbite-svelte'
+
+  let location = true
+  let pick1 = false
+  let pick2 = false
+  let pick3 = false
+</script>
+
+{#if location}
+  <div class="flex flex-col h-full">
+    <div class="text-white text-5xl p-2">
+      Select Location
+    </div>
+    <div class="overflow-x-scroll flex flex-row">
+      <div class="space-y-4 mt-4 mx-2 bg-slate-800">
+        <div>
+          <img src="shrek.jpg" alt="">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>         
+        </div>
+      </div>
+      <div class="space-y-4 mt-4 mx-2 bg-slate-800">
+        <div>
+            <img src="shrek.jpg" alt="">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+      </div>
+      <div class="space-y-4 mt-4 mx-2 bg-slate-800">
+        <div>
+            <img src="shrek.jpg" alt="">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+      </div>
+      <div class="space-y-4 mt-4 mx-2 bg-slate-800">
+        <div>
+            <img src="shrek.jpg" alt="">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-grow justify-center items-end mb-40">
+      <Button class="bg-cyan-500 py-4 px-16 text-white text-xl rounded-lg" on:click={() => {
+        location = false
+        pick1 = true
+        }}>
+        Next
+      </Button>
+    </div>
+  </div>
+{:else if pick1}
+  <div class="text-white text-5xl">page2</div>
+  <Button class="bg-cyan-500 py-4 px-16 text-white text-xl rounded-lg" on:click={() => {
+    pick1 = false
+    pick2 = true
+  }}>
+    Next
+  </Button>
+{:else if pick2}
+  <div class="text-white text-5xl">page3</div>
+  <Button class="bg-cyan-500 py-4 px-16 text-white text-xl rounded-lg" on:click={() => {
+    pick2 = false
+    pick3 = true
+  }}>
+    Next
+  </Button>
+{/if}
